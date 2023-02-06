5239d834dd29cfc84103a5e0c36d12dff25da903
Wonny-ing <h.wonny11@gmail.com>
2023-02-06 20:15:15 +0900

    Fix: Router.tsx

--- src/homework/01/index.tsx
+++ src/homework/01/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
 
 const Homework = () => {
-  return <>hello</>;
+  return (
+    <div>
+      <nav>
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+        <Link to='product'>
+          <button>Product</button>
+        </Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Homework;
